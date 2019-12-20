70bfebab96be15303d929dd5a09af95eee72ac24
zcfrank1st <zhchaos@gmail.com>
2019-12-20 09:12:00 +0800

    structure done need refactor

--- src/main/java/com/chaos/graphviz/flow/FlowCraft.java
+++ src/main/java/com/chaos/graphviz/flow/FlowCraft.java
@@ -1,10 +1,48 @@
 package com.chaos.graphviz.flow;
 
+import com.chaos.graphviz.flow.jj.FlowDot;
+import com.chaos.graphviz.flow.jj.ParseException;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import freemarker.template.TemplateExceptionHandler;
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
+import guru.nidi.graphviz.model.MutableGraph;
+import guru.nidi.graphviz.parse.Parser;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author chaos
  */
 public class FlowCraft {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, TemplateException, ParseException {
+        Configuration cfg = new Configuration(Configuration.VERSION_2_3_29);
+        cfg.setDirectoryForTemplateLoading(new File(FlowCraft.class.getResource("flow.ftlh").getPath().replace("flow.ftlh", "")));
+        cfg.setDefaultEncoding("UTF-8");
+        cfg.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER);
+        cfg.setLogTemplateExceptions(false);
+        cfg.setWrapUncheckedExceptions(true);
+        cfg.setFallbackOnNullLoopVariable(false);
 
+        FlowDot flowDot = new FlowDot(new ByteArrayInputStream("".getBytes()));
+        flowDot.build();
+
+
+        Map root = new HashMap();
+        root.put("nodes", FlowDot.nodes);
+
+        Template temp = cfg.getTemplate("flow.ftlh");
+        StringWriter writer = new StringWriter();
+        temp.process(root, writer);
+
+        MutableGraph g = new Parser().read(writer.toString());
+        Graphviz.fromGraph(g).render(Format.SVG).toFile(new File(""));
     }
 }
---
+++ src/test/java/FreeMarkerTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class FreeMarkerTest {
+
+    @Test
+    public void testFreeMarker() {
+
+    }
+}
